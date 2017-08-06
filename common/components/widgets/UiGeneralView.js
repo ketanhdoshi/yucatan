c64e3e943f6142a4748544e8b368fde7516091f4
ketan.doshi <ketan.doshi@ILPT528.ICERTIS.COM>
2017-08-06 21:53:13 +0530

    Added SmallBox as a HTML/CSS component

--- common/components/widgets/UiGeneralView.js
+++ common/components/widgets/UiGeneralView.js
@@ -4,7 +4,11 @@
 import React, { PropTypes } from 'react'
 
 import s from '../../scss/InfoBox.scss'
+import t from '../../scss/SmallBox.scss'
 
+// -----------------------------------------------------------------
+// InfoBox component
+// -----------------------------------------------------------------
 const InfoBox = (props) => {
     let progressStyle = { width: props.pct };
     return (
@@ -25,15 +29,49 @@ const InfoBox = (props) => {
     )
 }
 
+// -----------------------------------------------------------------
+// SmallBox component
+// -----------------------------------------------------------------
+const SmallBox = (props) => {
+    let supStyle = { fontSize: props.fs };
+    return (
+        <div className={t.smallBox + " " + props.bg}>
+            <div className={t.inner}>
+                <h3>{props.value}<sup style={supStyle}>{props.ss}</sup></h3>
+
+                <p>{props.title}</p>
+            </div>
+            <div className={t.icon}>
+                <i className={"ion " + props.icon}></i>
+            </div>
+            <a href="#" className={t.smallBoxFooter}>
+                More info <i className="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+     )
+}
+
+// -----------------------------------------------------------------
+// This component constructs the overall page. It is not meant to be
+// a reusable widget, but an orchestrator for the widgets on this
+// page.
+// -----------------------------------------------------------------
 const UiGeneralView = () => {
     return (
-        <div className="row">
-            <div className="col-md-3 col-sm-6 col-xs-12">
-                <InfoBox bg="bg-blue" icon="fa-bookmark-o" title="Bookmarks" value="283" pct='50%' desc="50% Increase in 30 Days"/>
-            </div> {/* /.col */}        
-            <div className="col-md-3 col-sm-6 col-xs-12">
-                <InfoBox bg="bg-green" icon="fa-desktop" title="Users" value="47865" pct='15%' desc="15% Improvement"/>
-            </div> {/* /.col */}        
+        <div>
+            <div className="row">
+                <div className="col-md-3 col-sm-6 col-xs-12">
+                    <InfoBox bg="bg-blue" icon="fa-bookmark-o" title="Bookmarks" value="283" pct='50%' desc="50% Increase in 30 Days"/>
+                </div> {/* /.col */}        
+                <div className="col-md-3 col-sm-6 col-xs-12">
+                    <InfoBox bg="bg-green" icon="fa-desktop" title="Users" value="47865" pct='15%' desc="15% Improvement"/>
+                </div> {/* /.col */}        
+            </div>
+            <div className="row">
+                <div className="col-lg-3 col-xs-6">
+                    <SmallBox bg="bg-green" title="Bounce Rate" value="43" ss='%' icon='ion-stats-bars' fs='20px'/>
+                </div> {/* /.col */} 
+            </div>
         </div>
     )
 }
